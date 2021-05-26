bf1fb3eeed5955c9555adb96b78dafac8e693a52
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-05-26 16:57:36 +0300

    comments are removed

--- tesler-doc-tests/src/test/java/io/tesler/test/UIIT.java
+++ tesler-doc-tests/src/test/java/io/tesler/test/UIIT.java
@@ -84,12 +84,12 @@ public class UIIT {
 		Configuration.browserSize = "1440x768";
 
 		Login();
-//		TestInput();
-//		TestDictionary();
-//		TestNumber();
-//		TestDate();
-//		TestPickList();
-//		TestFilter();
+		TestInput();
+		TestDictionary();
+		TestNumber();
+		TestDate();
+		TestPickList();
+		TestFilter();
 	}
 
 	private void Login() {
